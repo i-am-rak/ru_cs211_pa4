1be1733c67dd97b96232c51bf0582a3d559ba73f
raknahsa <raknahsa@gmail.com>
2016-11-15 23:18:46 -0500

    Add parsing
    Parse the .text to have a string which only contains the machine code

--- y86dis.c
+++ y86dis.c
@@ -12,7 +12,7 @@ int main(int argc, char ** argv){
     //FILE *op;
 
     char buff[1000];
-    char buff2[1000];
+    //char buff2[1000];
     
     fp = fopen(argv[1],"r");
     
@@ -21,14 +21,34 @@ int main(int argc, char ** argv){
         return 0;
     }
 
-    int i = 0;
+    int textFound = 0;
+    int i = 5;
     while(fgets(buff,1000, (FILE*)fp) != NULL){
         if(buff[0] == '.' && buff[1] == 't'  && buff[2] == 'e' && buff[3] == 'x' && buff[4] == 't'){
-            buff2 = buff+5;
+            //buff2 = buff;
+            textFound = 1;
             //printf("%s", buff+5);
             break;
         }
     }
+
+    if(textFound == 0){
+        printf("ERROR: <.text not found>\n");
+        return 0;
+    }
+
+    while(isspace(buff[i])){
+        i++;
+    }
+    while(!(isspace(buff[i]))){
+        i++;
+    }
+
+    while(isspace(buff[i])){
+        i++;
+    }
+
+    printf("Machine code %s", buff + i);
 
     fclose(fp);
     return 0;
