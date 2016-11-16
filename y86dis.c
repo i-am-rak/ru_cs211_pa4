4c6b8f2fafae0c8a3fbfc8d3d8f09b2f817db78b
raknahsa <raknahsa@gmail.com>
2016-11-16 17:22:08 -0500

    Add the rest of the y86 commands
    Add the operations, jumps, call, return, push, pop, read, write and
    movsbl. Operations include add, sub, and, xor, mull, cmpl.

--- y86dis.c
+++ y86dis.c
@@ -19,44 +19,165 @@ void doHalt(char input){
         printf("ERROR <wonky>");
 }
 
-void dorrmovl(char input, char arg1, char arg2){
+void dorrmovl(char input, char * bitval){
     if(input == '0'){
-       printf("rrmovl r%c,r%c\n",arg1,arg2);
+        printf("rrmovl r%c,r%c\n",bitval[0],bitval[1]);
     }
     else
         printf("ERROR <wonky>\n");
 }
 
-void doirmovl(char input, char arg1, char arg2){
-    if(input == '0' && arg1 == 'f'){
-        printf("irmovl f,r%c\n",arg2);
+void doirmovl(char input,char * bitval){
+
+    if(input == '0' && bitval[0] == 'f'){
+        printf("irmovl 0x%s,r%c\n",bitval+2,bitval[1]);
     }
     else
         printf("ERROR <wonky>\n");
 
 }
 
-void dormmovl(char input, char arg1, char arg2){
+void dormmovl(char input,char * bitval){
     if(input == '0'){
-       printf("rmmovl r%c,D(r%c)\n",arg1,arg2);
+        printf("rmmovl r%c,0x%s(r%c)\n",bitval[0],bitval+2,bitval[1]);
     }
     else
         printf("ERROR <wonky>\n");
 }
 
-void domrmovl(char input, char arg1, char arg2){
+void domrmovl(char input,char * bitval){
     if(input == '0'){
-       printf("mrmovl D(r%c),r%c\n",arg1,arg2);
+        printf("mrmovl 0x%s(r%c),r%c\n",bitval+2, bitval[0],bitval[1]);
     }
     else
         printf("ERROR <wonky>\n");
 }
 
+void doOper(char input, char * bitval){
+    switch(input){
+        case '0':
+            printf("addl r%c,r%c\n", bitval[0], bitval[1]);
+            break;
+        case '1':
+            printf("subl r%c,r%c\n", bitval[0], bitval[1]);
+            break;
+        case '2':
+            printf("andl r%c,r%c\n", bitval[0], bitval[1]);
+            break;
+        case '3':
+            printf("xorl r%c,r%c\n", bitval[0], bitval[1]);
+            break;
+        case '4':
+            printf("mull r%c,r%c\n", bitval[0], bitval[1]);
+            break;
+        case '5':
+            printf("cmpl r%c,r%c\n", bitval[0], bitval[1]);
+            break;  
+        default:  
+            printf("ERROR <wonky>\n"); 
+            break;
+    }
+}
 
+
+void doJmp(char input, char * bitval){
+    char dest [8];
+    strncpy(dest, bitval,8);
+    switch(input){
+        case '0':
+            printf("jmp 0x%s\n", dest);
+            break;
+        case '1':
+            printf("jle 0x%s\n", dest);
+            break;
+        case '2':
+            printf("jl 0x%s\n", dest);
+            break;
+        case '3':
+            printf("je 0x%s\n", dest);
+            break;
+        case '4':
+            printf("jne 0x%s\n", dest);
+            break;
+        case '5':
+            printf("jge 0x%s\n", dest);
+            break;
+        case '6':
+            printf("jg 0x%s\n", dest);
+            break;
+        default:
+            printf("ERROR <wonky>\n"); 
+            break;
+    }
+}
+
+void doCall(char input, char * bitval){
+    char dest[8];
+    strncpy(dest,bitval,8);
+    if(input == '0'){
+        printf("call 0x%s\n", dest);
+    }
+    else
+        printf("ERROR <wonky>\n"); 
+}
+
+void doRet(char input){
+    if(input == '0')
+        printf("ret\n");
+    else
+        printf("ERROR <wonky>\n"); 
+}
+
+void doPushl(char input, char * bitval){
+    if(input == '0'&& bitval[1] == 'f'){
+        printf("pushl r%c\n",bitval[0]);
+    }
+    else
+        printf("ERROR <wonky>\n");
+}
+
+void doPopl(char input, char * bitval){
+    if(input == '0'&& bitval[1] == 'f'){
+        printf("popl r%c\n",bitval[0]);
+    }
+    else
+        printf("ERROR <wonky>\n");
+}
+
+void doReadX(char input, char * bitval){
+    if(input == '0' && bitval[1] == 'f'){
+        printf("readb 0x%s(r%c)\n",bitval+2,bitval[0]);
+    }
+    else if(input == '1' && bitval[1] == 'f'){
+        printf("readl 0x%s(r%c)\n",bitval+2,bitval[0]);
+    }
+     else
+        printf("ERROR <wonky>\n");
+}
+
+void doWriteX(char input, char * bitval){
+    if(input == '0' && bitval[1] == 'f'){
+        printf("writeb 0x%s(r%c)\n",bitval+2,bitval[0]);
+    }
+    else if(input == '1' && bitval[1] == 'f'){
+        printf("writel 0x%s(r%c)\n",bitval+2,bitval[0]);
+    }
+    else
+        printf("ERROR <wonky>\n");
+}
+
+void doMovsbl(char input, char * bitval){
+    if(input == '0'){
+        printf("movsbl 0x%s(r%c),r%c\n",bitval+2,bitval[1], bitval[0]);
+    }
+    else
+        printf("ERROR <wonky>\n");
+}
 
 int main(int argc, char ** argv){
     if(argv[1][0] == '-' && argv[1][1] == 'h'){
-        printf("y86dis [-h] <y86 input file> \n Input is a *.y86 file");
+        printf("y86dis [-h] <y86 input file> \nInput is a *.y86 file\n");
+        return 0;
     }
 
     FILE *fp;
@@ -99,28 +220,55 @@ int main(int argc, char ** argv){
     while(isspace(buff[i])){
         i++;
     }
-
+    printf("\nMachine code : %s\n", buff+i);
+    printf("Assembly code  : \n");
     //int x = strlen(buff);
     int size = strlen(buff);
     //printf("%d\n", size);
     //printf("%c ,%d,  Machine code %s",buff[i],x,buff + i);
     fclose(fp);
-
+    int line = 0;
     int notHalted = 1;
     do{
+        line++;
         char opcode = buff[i];
         if(isspace(opcode)){
             return 0;
         }
+        printf("%d: ", line);
         char fncode = buff[i+1];
-        char argcode = 0;
-        char arg2code = 0;
+        //char argcode = 0;
+        //char arg2code = 0;
+        char * bit = malloc(13);
+        int j = 0;
+        for(j =0; j< 13; j++){
+            bit[j] = '\0';
+        }
         //printf("%d",(size - i)); 
         if((size - i) > 2){
-            argcode = buff[i+2];
-            arg2code = buff[i+3];
+            bit[0] = buff[i+2];
+            bit[1] = buff[i+3];
         }
+        if((size - i) > 4){
+            bit[2] = buff[i+4];
+            bit[3] = buff[i+5];
 
+            if((size - i) > 6){
+                bit[4] = buff[i+6];
+                bit[5] = buff[i+7];
+            
+                if((size - i) > 8){
+                    bit[6] = buff[i+8];
+                    bit[7] = buff[i+9];
+            
+                    if((size - i) > 10){    
+                        bit[8] = buff[i+10];
+                        bit[9] = buff[i+11];
+                    }
+                }
+            }
+        } 
+        
         switch(opcode){
             case '0':
                 doNoOp(fncode);
@@ -129,23 +277,59 @@ int main(int argc, char ** argv){
             case '1':
                 doHalt(fncode);
                 i = i+2;
-                notHalted = 0;
+                //notHalted = 0;
                 break;
             case '2':
-                dorrmovl(fncode,argcode,arg2code);
+                dorrmovl(fncode,bit);
                 i = i+4;
                 break;
             case '3':
-                doirmovl(fncode, argcode, arg2code);
-                i = i+4;
+                doirmovl(fncode,bit);
+                i = i+12;
                 break;
             case '4':
-                dormmovl(fncode, argcode, arg2code);
-                i = i+4;
+                dormmovl(fncode,bit);
+                i = i+12;
                 break;
             case '5':
-                domrmovl(fncode, argcode, arg2code);
+                domrmovl(fncode,bit);
+                i = i+12;
+                break;
+            case '6':
+                doOper(fncode, bit);
                 i = i+4;
+                break;
+            case '7':
+                doJmp(fncode, bit);
+                i = i+10;
+                break;
+            case '8':
+                doCall(fncode, bit);
+                i = i+10;
+                break;
+            case '9':
+                doRet(fncode);
+                i = i+2;
+                break;
+            case 'a':
+                doPushl(fncode, bit);
+                i = i+4;
+                break;
+            case 'b':
+                doPopl(fncode, bit);
+                i = i+4;
+                break;
+            case 'c':
+                doReadX(fncode, bit);
+                i = i+12;
+                break;
+            case 'd':
+                doWriteX(fncode, bit);
+                i = i+12;
+                break;
+            case 'e':
+                doMovsbl(fncode, bit);
+                i = i+12;
                 break;
             default:
                 printf("ERROR: <Unrecognized opcode %c>\n", opcode);
